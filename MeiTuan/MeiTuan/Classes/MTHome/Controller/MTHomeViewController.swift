db6bdf54d5412c9dbe7c8811d44535c42277cc36
kfzx-lijun <lijun02@sdc.icbc.com.cn>
2017-02-25 11:12:06 +0800

    uipopover添加

--- MeiTuan/MeiTuan/Classes/MTHome/Controller/MTHomeViewController.swift
+++ MeiTuan/MeiTuan/Classes/MTHome/Controller/MTHomeViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class MTHomeViewController: UIViewController {
 
+    var cateVc = UIViewController();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         // 0. 设置导航栏, 背景图片
         navigationController?.navigationBar.setBackgroundImage(UIImage(named: "bg_navigationBar_normal"), for: .default);
         
@@ -32,7 +34,7 @@ class MTHomeViewController: UIViewController {
         navigationItem.leftBarButtonItems = [logoItem,allCateItem.item,placeItem.item,sortItem.item];
     }
     
-    func createItem(icon: UIImage?, title:String, subTitle:String) ->(item:UIBarButtonItem, menuView:MTButtonItem ) {
+    func createItem(icon: UIImage?, title:String, subTitle:String) ->(item:UIBarButtonItem, menuView:MTButtonItem) {
         let itemView = MTButtonItem.buttonItemView(icon: icon, title: title, subTitle: subTitle);
         let customItem = UIBarButtonItem(customView: itemView);
         return (customItem, itemView);
@@ -41,11 +43,25 @@ class MTHomeViewController: UIViewController {
 
 extension MTHomeViewController{
     func category() {
-        print("category");
+        let item : UIBarButtonItem = navigationItem.leftBarButtonItems![1];
+        let itemView = item.customView as! MTButtonItem;
+        
+        cateVc.modalPresentationStyle = .popover;
+        cateVc.popoverPresentationController?.sourceView = itemView;
+        cateVc.popoverPresentationController?.sourceRect = itemView.bounds;
+        cateVc.popoverPresentationController?.backgroundColor = UIColor.yellow;
+        present(cateVc, animated: true, completion: nil);
     }
     
     func address() {
-         print("address");
+        
+        let item : UIBarButtonItem = navigationItem.leftBarButtonItems![2];
+        let itemView = item.customView as! MTButtonItem;
+        cateVc.modalPresentationStyle = .popover;
+        cateVc.popoverPresentationController?.sourceView = itemView;
+        cateVc.popoverPresentationController?.sourceRect = itemView.bounds;
+        cateVc.popoverPresentationController?.backgroundColor = UIColor.blue;
+        present(cateVc, animated: true, completion: nil);
     }
     
     func sort() {
